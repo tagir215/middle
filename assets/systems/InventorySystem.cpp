b562892ea0daaa1facc5f23dd71ca2c3dcd5b1a4
tagir215 <tagir.tsarkov@gmail.com>
2026-02-08 20:47:07 +0200

    inventory

--- assets/systems/InventorySystem.cpp
+++ assets/systems/InventorySystem.cpp
@@ -4,8 +4,16 @@
 #include "Inventory.h"
 #include "LoopSociety.h"
 #include "middle_shape_utils.h"
+#include "Rectangle.h"
+#include "Position.h"
 
 class InventorySystem : public middle::MiddleGameplaySystem {
+
+public:
+	InventorySystem() {
+		systemModeType = middle::SystemModeType::ENGINE;
+	}
+
 	void update(middle::GameState* gameState) override {
 		middle::loopInstances(gameState, [gameState](int i, middle::Shape& shape) {
 			auto inventory = middle::getComponent<components::Inventory>(shape);
@@ -16,6 +24,36 @@ class InventorySystem : public middle::MiddleGameplaySystem {
 			std::vector<middle::Id>items;
 			middle::getChildren(gameState, shape.id, items);
 
+			Vector3 inventoryPosition = middle::getShapePosition(gameState, shape.id.index);
+			const float ymargin = 10;
+			const float xmargin = 4;
+
+			// calculate total height
+			float totalHeight = 0;
+			float totalWidth = 0;
+			for (middle::Id& childId : items) {
+				middle::Shape& child = middle::getShape(gameState, childId.index);
+				auto rect = middle::getComponent<components::Rectangle>(child);
+				totalHeight += rect->height + ymargin;
+				totalWidth = rect->width + xmargin > totalWidth ? rect->width + xmargin : totalWidth;
+			}
+
+			Vector3 pos = inventoryPosition;
+			pos.z += totalHeight * 0.5f;
+			auto inventoryRect = middle::getComponent<components::Rectangle>(shape);
+			inventoryRect->width = totalWidth;
+			inventoryRect->height = totalHeight;
+
+			for (middle::Id& childId : items) {
+				middle::Shape& child = middle::getShape(gameState, childId.index);
+				auto rect = middle::getComponent<components::Rectangle>(child);
+				auto position = middle::getComponent<components::Position>(child);
+				pos.z -= rect->height * 0.5f + ymargin * 0.5f;
+				position->posX = pos.x;
+				position->posY = pos.y;
+				position->posZ = pos.z;
+				pos.z -= rect->height * 0.5f + ymargin * 0.5f;
+			}
 
 			});
 	}
