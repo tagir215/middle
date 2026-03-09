63cb4e1ab61cf2f20f3e02b7dcd393e8f4b5ee8f
tagir215 <tagir.tsarkov@gmail.com>
2026-03-09 20:13:03 +0200

    mouseGrabbing refactor

--- assets/systems/MouseGrabbingSystem.cpp
+++ assets/systems/MouseGrabbingSystem.cpp
@@ -19,9 +19,53 @@ namespace MouseGrabbingSystem {
 			systemUpdateType = middle::SystemUpdateType::PREFRAME;
 			systemModeType = middle::SystemModeType::EDITOR;
 		}
-		void init(middle::GameState* gameState) {
 
+		components::CompCache* grabbableCache;
+		components::CompCache* placableCache;
+
+
+		void init(middle::GameState* gameState) {
+			grabbableCache = middle::newCompCache(gameState);
+			grabbableCache->addType<components::MouseGrabbable>();
+			grabbableCache->addType<components::Position>();
+			placableCache = middle::newCompCache(gameState);
+			placableCache->addType<components::PlacementComponent>();
+			placableCache->addType<components::Position>();
 		}
+
+		void dragging(middle::GameState* gameState, middle::Shape& shape) {
+			Vector3 pos = middle::getShapePosition(gameState, shape.id.index);
+			auto grid = middle::getComponent<components::GridElement>(shape);
+
+			if (!grid) {
+				Vector3 cameraPos = gameState->editorState.camera.position;
+				float objYDistance = std::abs(pos.y - cameraPos.y);
+				float yDistance = std::abs(cameraPos.y);
+				if (yDistance == 0)
+					yDistance = 0.001f;
+				Vector3 xzVel = Vector3Scale(gameState->input.mouseXZ_PlaneVelocity, objYDistance / yDistance);
+				dragShape(gameState, shape.id.index, xzVel);
+			}
+			else {
+				Vector3 targetPos = gameState->input.mouseXZ_PlanePos;
+				middle::moveShape(gameState, shape.id.index, targetPos - pos);
+			}
+		}
+
+		void releasing(middle::GameState* gameState) {
+			// update move action with new positions for redo to work
+			middle::EditorActionContainer* lastAction = gameState->editorState.actionHistory.back().get();
+			auto lastMoveAction = static_cast<middle::EditorActionMove*>(lastAction);
+			assert(lastMoveAction);
+			lastMoveAction->newPositions.resize(lastMoveAction->selectedShapes.size());
+			for (int j = 0; j < lastMoveAction->selectedShapes.size(); ++j) {
+				auto& selectedShape = middle::getShape(gameState, lastMoveAction->selectedShapes[j]);
+				auto position = middle::getComponent<components::Position>(selectedShape);
+				lastMoveAction->newPositions[j] = { position->posX, position->posY, position->posZ };
+			}
+			gameState->editorState.grabbing = false;
+		}
+
 		void update(middle::GameState* gameState) override {
 
 			// setup editor action for movement
@@ -31,16 +75,12 @@ namespace MouseGrabbingSystem {
 				return;
 			}
 
-			for (int i = 0; i < gameState->shapes.size(); ++i) {
-				middle::Shape& shape = gameState->shapes[i];
-				auto grabbable = middle::getComponent<components::MouseGrabbable>(shape);
-				auto placable = middle::getComponent<components::PlacementComponent>(shape);
-				auto position = middle::getComponent<components::Position>(shape);
-				if (!position)
-					continue;
-				if (!grabbable && !placable)
-					continue;
-
+			auto grabbableIt = grabbableCache->begin<components::MouseGrabbable>();
+			auto positionIt = grabbableCache->begin<components::Position>();
+			for (int i = 0; i < grabbableCache->getSize(); ++i) {
+				auto& shape = middle::getShape(gameState, grabbableCache->relevantIdVector[i].index);
+				auto grabbable = *grabbableIt;
+				auto position = *positionIt;
 
 				if (grabbable && middle::isShapeSelected(gameState, i) && gameState->input.grabDown) {
 					grabbable->grabbing = true;
@@ -49,63 +89,32 @@ namespace MouseGrabbingSystem {
 					grabbable->grabbing = false;
 				}
 
-				// remove placement component after clicking
-				if (placable && gameState->input.mouseClicked) {
-					middle::queueComponentDeletion<components::PlacementComponent>(gameState, shape.id);
-					std::vector<middle::Id>members;
-					middle::getAllChildren(gameState, shape.id, members);
-					for (middle::Id& childId : members) {
-						middle::Shape& child = middle::getShape(gameState, childId.index);
-						middle::queueComponentDeletion<components::PlacementComponent>(gameState, childId);
-					}
-				}
-
-				if ((grabbable && grabbable->grabbing) || (placable && placable->grabbing)) {
-					Vector3 pos = middle::getShapePosition(gameState, i);
-					auto grid = middle::getComponent<components::GridElement>(shape);
-
-					if (!grid) {
-						Vector3 cameraPos = gameState->editorState.camera.position;
-						float objYDistance = std::abs(pos.y - cameraPos.y);
-						float yDistance = std::abs(cameraPos.y);
-						if (yDistance == 0)
-							yDistance = 0.001f;
-						Vector3 xzVel = Vector3Scale(gameState->input.mouseXZ_PlaneVelocity, objYDistance / yDistance);
-						dragShape(gameState, i, xzVel);
-					}
-					else {
-						Vector3 targetPos = gameState->input.mouseXZ_PlanePos;
-						middle::moveShape(gameState, i, targetPos - pos);
-					}
-				}
-
-				if (gameState->input.grabReleased && gameState->editorState.grabbing) {
-					// update move action with new positions for redo to work
-					middle::EditorActionContainer* lastAction = gameState->editorState.actionHistory.back().get();
-					auto lastMoveAction = static_cast<middle::EditorActionMove*>(lastAction);
-					assert(lastMoveAction);
-					lastMoveAction->newPositions.resize(lastMoveAction->selectedShapes.size());
-					for (int j = 0; j < lastMoveAction->selectedShapes.size(); ++j) {
-						auto& selectedShape = middle::getShape(gameState, lastMoveAction->selectedShapes[j]);
-						auto position = middle::getComponent<components::Position>(selectedShape);
-						lastMoveAction->newPositions[j] = { position->posX, position->posY, position->posZ };
-					}
-					gameState->editorState.grabbing = false;
-				}
-
-				if (gameState->input.grabReleased && gameState->editorState.selectCount == 1) {
-					unselect(gameState);
-				}
-
-				bool paused = gameState->paused && !gameState->editorState.doOneStep;
-				gameState->editorState.doOneStep = false;
-
-				if (paused || gameState->editorState.stepDir == -1)
-				{
-					gameState->editorState.doOneStep = false;
-					return;
+				if (grabbable->grabbing) {
+					dragging(gameState, shape);
 				}
 			}
+
+			auto placableIt = placableCache->begin<components::PlacementComponent>();
+			for (int i = 0; i < placableCache->getSize(); ++i) {
+				auto placable = *placableIt;
+				auto& shape = middle::getShape(gameState, placableCache->relevantIdVector[i].index);
+				middle::queueComponentDeletion<components::PlacementComponent>(gameState, shape.id);
+				std::vector<middle::Id>members;
+				middle::getAllChildren(gameState, shape.id, members);
+				for (middle::Id& childId : members) {
+					middle::Shape& child = middle::getShape(gameState, childId.index);
+					middle::queueComponentDeletion<components::PlacementComponent>(gameState, childId);
+				}
+
+				if (placable->grabbing) {
+					dragging(gameState, shape);
+				}
+			}
+
+			if (gameState->input.grabReleased && gameState->editorState.grabbing) {
+				releasing(gameState);
+			}
+
 		}
 	};
 
