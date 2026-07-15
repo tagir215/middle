acb32ee28782109124fe875f26af691d4c5b3e00
Tagir Tirkkonen <tagir.tirkkonen@gmail.com>
2026-07-15 20:26:56 +0300

    algutils

---
+++ include/alg_file_utils.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace bubequ {
+	enum class LinkType {
+		NONE, 
+		ADDITION,
+		MULTIPLICATION,
+		POWER,
+		EQUALS
+	};
+
+	enum class UnitType {
+		NONE,
+		CONSTANT,
+		VARIABLE
+	};
+
+	struct Scope {
+		std::vector<Scope>children;
+	};
+
+	struct Unit : public Scope{
+		UnitType type;
+		std::string label;
+		int value = -1;
+	};
+	struct Link : public Scope{
+		LinkType type;
+	};
+
+}
---
+++ src/program/alg_file_utils.cpp
@@ -0,0 +1,1 @@
+#include "alg_file_utils.h"
